17c065c5b150ef3cbfb386dc0a28e25fe2c75ca8
administrateur <administrateur@GERJMCK.m2iformation.local>
2025-09-24 13:34:48 +0200

    njsdfsdjnfqmjn

---
+++ terraform/main.tf
@@ -0,0 +1,92 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.32"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+# Variables
+variable "project" { default = "demo-js" }
+variable "app"     { default = "js-app" }
+variable "image"   { default = "node:18-alpine" }
+
+# Namespace
+resource "kubernetes_namespace_v1" "ns" {
+  metadata {
+    name = var.project
+  }
+}
+
+# Deployment JS App
+resource "kubernetes_deployment_v1" "deploy" {
+  metadata {
+    name      = var.app
+    namespace = var.project
+    labels = {
+      app = var.app
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = var.app
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = var.app
+        }
+      }
+
+      spec {
+        container {
+          name  = var.app
+          image = var.image
+
+          command = [
+            "node",
+            "-e",
+            "require('http').createServer((req,res)=>res.end('Hello from JS App!')).listen(3000)"
+          ]
+
+          port {
+            container_port = 3000
+          }
+        }
+      }
+    }
+  }
+}
+
+# Service exposing port 3000
+resource "kubernetes_service_v1" "svc" {
+  metadata {
+    name      = var.app
+    namespace = var.project
+  }
+
+  spec {
+    selector = {
+      app = var.app
+    }
+
+    port {
+      name        = "http"
+      port        = 3000
+      target_port = 3000
+    }
+
+    type = "ClusterIP"
+  }
+}
